8d35a0089f6bf996cc58d245497110965f10dea5
Mahmoud Khaled Ali <mahmoud.k.a@outlook.com>
2018-07-27 20:07:27 +0200

    Add search endpoint

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,33 +1,56 @@
 const pool = require('../utils/sqlConnector.js');
+const _ = require('lodash');
 
-exports.productSearch = async (req, res) => {
+const resultsPerPage = 6;
+
+exports.productSearch = async (req, res, next) => {
   let connection;
 
-  const platformQuery = `SELECT product.*, AVG(reviews.score) AS avg_score
-  FROM product LEFT JOIN reviews ON product.idproduct = reviews.idproduct
+  const platformQuery = `SELECT COUNT(*) as count, product.*, AVG(review.score) AS avg_score
+  FROM product LEFT JOIN review ON product.idproduct = review.idproduct
   WHERE platform = ? AND name LIKE ? GROUP BY product.idproduct ORDER BY
-  ${req.body.field} ${req.body.order}`;
+  ${req.body.field} ${req.body.order}
+  LIMIT ? OFFSET ?`;
 
-  const platformQueryParameters = [req.body.platform, '%' + req.body.name + '%'];
+  const platformQueryParameters = [
+    req.body.platform,
+    '%' + req.body.name + '%',
+    resultsPerPage,
+    (req.body.pageNo - 1) * resultsPerPage
+  ];
 
-  const query = `SELECT product.*, AVG(reviews.score) AS avg_score
-  FROM product LEFT JOIN reviews ON product.idproduct = reviews.idproduct
+  const query = `SELECT COUNT(*) as count, product.*, AVG(review.score) AS avg_score
+  FROM product LEFT JOIN review ON product.idproduct = review.idproduct
   WHERE name LIKE ? GROUP BY product.idproduct ORDER BY
-  ${req.body.field} ${req.body.order}`;
+  ${req.body.field} ${req.body.order}
+  LIMIT ? OFFSET ?`;
 
-  const queryParameters = ['%' + req.body.name + '%'];
+  const queryParameters = [
+    '%' + req.body.name + '%',
+    resultsPerPage,
+    (req.body.pageNo - 1) * resultsPerPage
+  ];
 
   try {
     connection = await pool.getConnection();
     let searchResults;
 
     if (req.body.platform == -1) {
-      searchResults = connection.query(query, queryParameters);
+      searchResults = await connection.query(query, queryParameters);
     } else {
-      searchResults = connection.query(platformQuery, platformQueryParameters);
+      searchResults = await connection.query(platformQuery, platformQueryParameters);
     }
 
-    res.json(searchResults);
+    if (!searchResults.length) {
+      return res.send('No results matching your search');
+    }
+
+    const response = {
+      count: searchResults[0].count,
+      products: searchResults.map(result => _.omit(result, 'count'))
+    }
+
+    res.json(response);
   } catch (err) {
     next(err);
   } finally {
